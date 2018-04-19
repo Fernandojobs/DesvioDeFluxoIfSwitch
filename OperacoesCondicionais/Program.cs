0b8355b741eea7c204c9160b3152520cb5d67e45
Fernando Jobs <fernandoocarmo@outlook.com>
2018-04-19 11:11:37 -0300

    Declaração de Varáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,18 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentario in-line (em linha)
+            /*
+                Comentario in-block (em bloco)
+            */
+            // declaração da variavel opreração. Ela será do tipo inteiro 
+            int operacao;
+
+            //declaração das variavel n1 e n2. Elas serão do tipo double
+            double n1, n2;
+            
+
+
         }
     }
 }
